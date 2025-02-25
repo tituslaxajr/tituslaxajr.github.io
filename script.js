7da2d70f37f7834d826afe138f91ee38fb01c65b
tituslaxajr <titus.laxa.jr@gmail.com>
2025-02-25 19:52:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const darkModeToggle = document.getElementById("dark-mode-toggle");
 
-    darkModeToggle.addEventListener("click", () => {
-        document.body.classList.toggle("dark-mode");
-    });
-});
